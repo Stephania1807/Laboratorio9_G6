a7f8b6549125a8af79651e7def2a549ae68741f6
Joau0824 <a20182162@pucp.edu.pe>
2022-06-21 19:35:55 -0500

    Correcion HTML

--- detalleLocacion/detalleLocacion.js
+++ detalleLocacion/detalleLocacion.js
@@ -3,5 +3,5 @@ $(document).ready(function () {
     // Metodo de obtención de parámetros
     const urlParams = new URLSearchParams(window.location.search);
     const idLocacion = urlParams.get('locacion');
-    
+
 });
