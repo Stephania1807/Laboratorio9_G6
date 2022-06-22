a3e4a4095001486a14c340f1a1a79d8b3a410cce
Joau0824 <a20182162@pucp.edu.pe>
2022-06-22 02:18:27 -0500

    pokemones

--- detalleLocacion/detalleLocacion.js
+++ detalleLocacion/detalleLocacion.js
@@ -19,7 +19,8 @@ $(document).ready(function () {
             tablaDinamic += "<tr>";
             tablaDinamic += "<td>" + (i + 1) + "</td>";
             tablaDinamic += "<td>"+ listapokemones2[i].name + "</td>";
-            tablaDinamic += "<td><a href='../detalleLocacion/detalleLocacion.html?locacion-area="+ (i+283)+"' class='btn btn-primary botonDetalle'>" +"Ver Pokemones" + "</a></td>";
+            let id = listapokemones2[i].url.split("/");
+            tablaDinamic += "<td><button class='btn btn-primary pokemones' value='" + listapokemones2[i].url +"'>" +"Ver Pokemones" + "</button></td>";
             tablaDinamic += "</tr>";
         }
         let regresar = data.region.url.split("/");
@@ -31,32 +32,30 @@ $(document).ready(function () {
         $("#labelLocacion").text("Locación:" +location);
         $("#labelRegion").text("Región:" +region);
         $("#tablaAreas").html(tablaDinamic);
+
     }).fail(function (e){
         console.log(e)
     });
+    $("#tablaAreas").on("click", ".pokemones",function (){
+        url = $(this).val();
+        $("#pokemons").empty();
+        $.get(url).done(function (data){
+            $("#areaSeleccionada").html("Pokemons a encontrarse en el area: "+data.name);
+            let listapokemones = data.pokemon_encounters;
+            for(let i = 0; i<listapokemones.length; i++){
+                $.get(listapokemones[i].pokemon.url).done(function (pikachu){
+                    console.log(pikachu);
+                    $("#pokemons").append("<div style='border: solid; margin-left: 50px; margin-right: 50px; margin-bottom: 50px' class='col-2'>" +
+                        "<img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/version\n" + "s/generation-v/black-white/animated/" +pikachu.id+".gif'> <br>" +pikachu.name +" </div>")
 
-});
+                }).fail(function (ee){
+                    console.log(ee);
+                })
 
+            }
 
-$(document).ready(function () {
-    // Metodo de obtención de parámetros
-    const urlParams = new URLSearchParams(window.location.search);
-    const idLocacion_area = urlParams.get('area');
-    let url = "https://pokeapi.co/api/v2/location-area/"+idLocacion_area;
-    $.ajax({
-        method:"GET",
-        url:url,
-    }).done(function (data){
-        let listapokemones3 = data.pokemon_encounters;
-        let card="";
-        for(let i = 0; i<listapokemones3.length; i++){
-            card +="<div>";
-            card +="</div>";
-        }
-        $("#pokemons").html(card);
-    }).fail(function (e){
-        console.log(e)
+        }).fail(function (e){
+            console.log(e);
+        });
     });
-
 });
-
